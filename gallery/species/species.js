c2ad07301f2a390d03b908a897ce0b705b809a7a
Achmet Tachsin <tachsinachmet@gmail.com>
2020-06-05 03:59:49 +0300

    json update

--- gallery/species/species.js
+++ gallery/species/species.js
@@ -21,123 +21,16 @@ function GET_INFO(theLink){
     window.open(theLink, '_blank').focus();
 }
 
-var Species = {
-    [Symbol.iterator] : function(){
-        let properties = Object.keys(this);
-        let count = 0;
-        isDone = false;
-    
-        let next = () => {
-            if(count >= properties.length) isDone = true;
-            return {done:isDone, value: this[properties[count++]]};
-            
-        }
-    
-        return {next};
-    },
-
-    ailurus_fulgens: {
-        scientific: "Ailurus fulgens",
-        image: "ailurus_fulgens.jpg",
-        credit: "Image credit: ARJUN THAPA, Institute of Zoology, Chinese Academy of Sciences.",
-        name: "Himalayan red panda",
-        link: "https://en.wikipedia.org/wiki/Red_panda",
-    },
-
-    ailurus_styani: {
-        scientific: "Ailurus styani",
-        image: "ailurus_styani.jpg",
-        credit: "Image credit: YUNFANG XIU, Straits (Fuzhou) Giant Panda Research and Exchange Center, China.",
-        name: "Chinese red panda",
-        link: "https://en.wikipedia.org/wiki/Red_panda",
-    },
-
-    ochotona_princeps: {
-        scientific: "Ochotona princeps",
-        image: "ochotona_princeps.jpg",
-        credit: "Image credit: KEITH KOHL",
-        name: "American pika",
-        link: "https://en.wikipedia.org/wiki/American_pika",
-    },
-	
-
-    ambystoma_mexicanum: {
-        scientific: "Ambystoma mexicanum",
-        image: "ambystoma_mexicanum.jpg",
-        credit: "Image credit: JAN-PETER KASPER/CORBIS",
-        name: "Mexican axolotl",
-        link: "https://en.wikipedia.org/wiki/Axolotl",
-    },
-
-    felis_margarita_thinobia: {
-        scientific: "Felis margarita thinobia",
-        image: "felis_margarita_thinobia.jpg",
-        credit: "Image credit: PAYMAN SAZESH",
-        name: "Turkestan sand cat",
-        link: "https://en.wikipedia.org/wiki/Felis_margarita_thinobia",
-    },
-
-    vulpes_zerda: {
-        scientific: "Vulpes zerda",
-        image: "vulpes_zerda.jpg",
-        credit: "Image credit: YVONNE N. in Willowick, USA",
-        name: "Fennec fox",
-        link: "https://en.wikipedia.org/wiki/Fennec_fox",
-    },
-
-    atelerix_albiventris: {
-        scientific: "Atelerix albiventris",
-        image: "atelerix_albiventris.png",
-        credit: "Image credit: ANYKA in Belgium",
-        name: "Four-toed hedgehog",
-        link: "https://en.wikipedia.org/wiki/Four-toed_hedgehog",
-    },
-
-    poecilotheria_metallica: {
-        scientific: "Poecilotheria metallica",
-        image: "poecilotheria_metallica.jpg",
-        credit: "Image credit: MLursus on flickr",
-        name: "Gooty sapphire ornamental",
-        link: "https://en.wikipedia.org/wiki/Poecilotheria_metallica",
-    },
-
-    pitohui_dichrous: {
-        scientific: "Pitohui dichrous",
-        image: "pitohui_dichrous.png",
-        credit: "Image credit: Markus Lilje on flickr",
-        name: "Hooded pitohui",
-        link: "https://en.wikipedia.org/wiki/Hooded_pitohui",
-    },
-
-    thalassoma_purpureum: {
-        scientific: "Thalassoma purpureum",
-        image: "thalassoma_purpureum.png",
-        credit: "Image credit: Inger Vandyke",
-        name: "Surge wrasse",
-        link: "https://en.wikipedia.org/wiki/Surge_wrasse",
-    },
-
-    phycodurus_eques: {
-        scientific: "Phycodurus eques",
-        image: "phycodurus_eques.jpg",
-        credit: "Image credit: James Rosindell",
-        name: "Leafy seadragon",
-        link: "https://en.wikipedia.org/wiki/Leafy_seadragon",
-    },
-}
-
 var randomList = [];
-for(obj of Species) randomList.push(obj);
-shuffle(randomList);
-
-let i = 0;
-for(obj in randomList){
-    $(".wrapper").append(
-        $("<div class='container' onclick=GET_INFO(`" + randomList[obj].link + "`)>").append(
-            $("<div class='text'>").text(randomList[obj].scientific)).append(
-            $("<img src='/gallery/species/images/" + randomList[obj].image + "' title='" + randomList[obj].credit + "'>")).append(
-            $("<p>").text(randomList[obj].name))
-    );
-    i++;
-}
-
+$.getJSON("/gallery/species/species.json", d => {
+    $.each(d, function(key, val){ randomList.push(val); });
+    shuffle(randomList);
+    for(obj in randomList){
+        $(".wrapper").append(
+            $("<div class='container' onclick=GET_INFO(`" + randomList[obj].link + "`)>").append(
+                $("<div class='text'>").text(randomList[obj].scientific)).append(
+                $("<img src='/gallery/species/images/" + randomList[obj].image + "' title='" + randomList[obj].credit + "'>")).append(
+                $("<p>").text(randomList[obj].name))
+        );
+    }
+});
--- getPushTime.js
+++ getPushTime.js
@@ -1,1 +1,1 @@
-const __PUSHTIME__ = "05/06/2020 03:36:17 AM";
+const __PUSHTIME__ = "05/06/2020 03:59:50 AM";

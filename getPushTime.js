3bc3fb8311e3170735617d863793fb55235853a7
Achmet Tachsin <tachsinachmet@gmail.com>
2020-06-11 12:48:39 +0300

    random

--- getPushTime.js
+++ getPushTime.js
@@ -1,1 +1,1 @@
-const __PUSHTIME__ = "11/06/2020 12:39:48 PM";
+const __PUSHTIME__ = "11/06/2020 12:48:39 PM";
--- random/random.js
+++ random/random.js
@@ -1,5 +1,12 @@
+var randomList = [];
+
 $.getJSON("/random/random.json", d => {
-    $.each(d, function(key, item){
-        console.log(item);
+    $.each(d.data, function(key, item){
+        randomList.push(item.url);
     });
+}).then(function(){
+    var randomItem = randomList[Math.floor(Math.random()*randomList.length)];
+    console.log("Random item is " + randomItem);
+    window.location = randomItem;
 });
+
